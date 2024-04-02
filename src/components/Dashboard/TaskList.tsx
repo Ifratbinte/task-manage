7fc0dd4c7c417bfda4cf4f09467a2a383c95fc2f
IfratBinte <ifratbinte@gmail.com>
2024-04-02 20:45:05 +0600

    responsive: fix responsive issue on table

--- src/components/Dashboard/TaskList.tsx
+++ src/components/Dashboard/TaskList.tsx
@@ -31,7 +31,7 @@ const TasksList: React.FC<Props> = ({tasks,  addTaskHandler}) => {
     <div className="bg-gray-100 p-4">
 
       {/* Task list Header */}
-      <div className="flex items-center justify-between mb-6">
+      <div className="flex flex-col sm:flex-row sm:items-center justify-between mb-6">
         <h2 className="text-xl font-semibold mb-4">Task List</h2>
         <div className="flex items-center gap-6">
           {/* Filter task */}
@@ -42,41 +42,43 @@ const TasksList: React.FC<Props> = ({tasks,  addTaskHandler}) => {
       </div>
 
       {/* Task list Table */}
-      <table className="border-collapse table-auto w-full text-sm">
-        <thead className='dark:bg-meta'>
-          <tr className='border-b dark:border-slate-600 font-medium p-4 text-slate-700 dark:text-slate-200 text-left text-base'>
-            <th className='pl-8 py-3'>Title</th>
-            <th className='px-8 pb-y'>Description</th>
-            <th className='pr-8 pb-y'>Status</th>
-            <th className='pr-8 pb-y'>Due Date</th>
-            <th className='pr-8 text-center'>Action</th>
-          </tr>
-        </thead>
-        <tbody className='bg-white dark:bg-slate-800'>
-          {/* task list container start */}
-          {tasks.map((task:any) => (
-            <tr key={task.title} className='border-b border-slate-100 dark:border-slate-700 text-slate-500 dark:text-slate-400 text-base'>
-              <td className="pl-8 py-3 flex items-center gap-3">
-                <input
-                  type="checkbox"
-                  checked={task.completed}
-                  onChange={handleToggleCompletion}
-                />
-                {task.title}
-              </td>
-              <td className='px-8 py-3'>{task.description}</td>
-              <td className="pr-8 py-3">{task.status}</td>
-              <td className="pr-8 py-3">{task.dueDate.toDateString()}</td>
-              <td className='pr-8 text-center'>
-                <button onClick={handleDelete} className="text-red-400 hover:text-red-600">
-                  <FaTrashAlt/>
-                </button>
-              </td>
+      <div className="overflow-x-auto lg:overflow-x-hidden">
+        <table className="border-collapse table-auto w-full text-sm">
+          <thead className='dark:bg-meta'>
+            <tr className='border-b dark:border-slate-600 font-medium p-4 text-slate-700 dark:text-slate-200 text-left text-base'>
+              <th className='pl-8 py-3'>Title</th>
+              <th className='px-8 pb-y'>Description</th>
+              <th className='pr-8 pb-y'>Status</th>
+              <th className='pr-8 pb-y'>Due Date</th>
+              <th className='pr-8 text-center'>Action</th>
             </tr>
-          ))}
-          {/* task list container end */}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className='bg-white dark:bg-slate-800'>
+            {/* task list container start */}
+            {tasks.map((task:any) => (
+              <tr key={task.title} className='border-b border-slate-100 dark:border-slate-700 text-slate-500 dark:text-slate-400 text-base'>
+                <td className="pl-8 py-3 flex items-center gap-3">
+                  <input
+                    type="checkbox"
+                    checked={task.completed}
+                    onChange={handleToggleCompletion}
+                  />
+                  {task.title}
+                </td>
+                <td className='px-8 py-3'>{task.description}</td>
+                <td className="pr-8 py-3">{task.status}</td>
+                <td className="pr-8 py-3">{task.dueDate.toDateString()}</td>
+                <td className='pr-8 text-center'>
+                  <button onClick={handleDelete} className="text-red-400 hover:text-red-600">
+                    <FaTrashAlt/>
+                  </button>
+                </td>
+              </tr>
+            ))}
+            {/* task list container end */}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
